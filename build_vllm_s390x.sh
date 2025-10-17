1b72615374cf829a364672ff1139f4002cd2a761
Nishan Acharya <Nishan.Acharya@ibm.com>
2025-10-17 08:51:47 +0200

    Add xsimd headers
    Signed-off-by: Nishan Acharya <Nishan.Acharya@ibm.com>

--- build_vllm_s390x.sh
+++ build_vllm_s390x.sh
@@ -47,6 +47,13 @@ cd ${CURDIR}
 #export PYARROW_VERSION=19.0.1
 git clone --recursive https://github.com/apache/arrow.git
 cd arrow/cpp
+
+# Patch Arrow to disable xsimd includes (avoid version compatibility issues)
+sed -i 's/#include <xsimd\/xsimd.hpp>/\/\/ #include <xsimd\/xsimd.hpp>/' \
+    src/arrow/util/bpacking_simd128_generated_internal.h
+sed -i 's/#include "arrow\/util\/bpacking_simd128_generated_internal.h"/\/\/ #include "arrow\/util\/bpacking_simd128_generated_internal.h"/' \
+    src/arrow/util/bpacking_simd_default.cc
+
 mkdir -p release
 cd release
 cmake -DCMAKE_BUILD_TYPE=Release \
