1986e5843335c121349683fa4c6382211d31d737
Baibhab Chakraborty <Baibhab.Chakraborty@ibm.com>
2026-09-23 19:14:59 +0200

    updated gccversion to 14

--- build_vllm_s390x.sh
+++ build_vllm_s390x.sh
@@ -16,12 +16,20 @@ export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-/usr/lib64:/usr/lib}
 # install development packages
 microdnf install -y \
     which procps findutils tar vim git gcc gcc-gfortran g++ gcc-c++ make patch zlib-devel \
+    gcc-toolset-14 gcc-toolset-14-binutils gcc-toolset-14-libatomic-devel \
     libjpeg-turbo-devel libtiff-devel libpng-devel libwebp-devel freetype-devel harfbuzz-devel \
     openssl-devel openblas openblas-devel autoconf automake libtool libzstd-devel cmake numpy libsndfile \
     clang clang-devel ninja-build perl-core llvm llvm-devel && \
     microdnf clean all
 
-pip install --no-cache -U pip setuptools wheel && \
+# Enable gcc-toolset-14
+source /opt/rh/gcc-toolset-14/enable
+export PATH=/opt/rh/gcc-toolset-14/root/usr/bin:$PATH
+export LD_LIBRARY_PATH=/opt/rh/gcc-toolset-14/root/usr/lib64:${LD_LIBRARY_PATH}
+export LIBRARY_PATH=/opt/rh/gcc-toolset-14/root/usr/lib64
+export PKG_CONFIG_PATH=/opt/rh/gcc-toolset-14/root/usr/lib64/pkgconfig:${PKG_CONFIG_PATH:-}
+
+pip install --no-cache -U pip "setuptools==80.10.2" wheel && \
 pip install --no-cache -U uv
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y && \
@@ -63,6 +71,7 @@ make install
 cd ../../python
 export PYARROW_PARALLEL=4
 export ARROW_BUILD_TYPE=release
+uv pip install "cython<3.3.0"
 uv pip install -r requirements-build.txt
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 python setup.py build_ext --build-type=$ARROW_BUILD_TYPE --bundle-arrow-cpp --inplace
@@ -90,7 +99,8 @@ pip download torch==2.10.0+cpu \
   -d ${WHEEL_DIR}
 
 # Download setuptools in the version range required by vllm (>=77.0.3,<81.0.0)
-pip download "setuptools>=77.0.3,<81.0.0" \
+VLLM_SETUPTOOLS_VERSION="80.10.2"
+pip download "setuptools==${VLLM_SETUPTOOLS_VERSION}" \
   --extra-index-url https://pypi.org/simple \
   -d ${WHEEL_DIR}
 
@@ -253,7 +263,15 @@ cd ${CURDIR}
 
 mkdir -p lapack
 mkdir -p OpenBLAS
-rm -f ${WHEEL_DIR}/setuptools-8[12]*.whl ${WHEEL_DIR}/setuptools-7[0-6]*.whl || true
+#rm -f ${WHEEL_DIR}/setuptools-8[12]*.whl ${WHEEL_DIR}/setuptools-7[0-6]*.whl || true
+SETUPTOOLS_WHEEL="${WHEEL_DIR}/setuptools-80.10.2-py3-none-any.whl"
+
+find "${WHEEL_DIR}" -maxdepth 1 -type f -name 'setuptools-*.whl' \
+    ! -path "${SETUPTOOLS_WHEEL}" \
+    -delete
+
+test -f "${SETUPTOOLS_WHEEL}"
+
 
 uv pip install ${WHEEL_DIR}/*.whl
 
